9879c3027cdc1ecdc6460f013e8067c03e5cf72a
juanar <JuanLuis.AntonRomero@zooplus.com>
2018-04-30 09:58:24 +0200

    Add GIT_DIR variable to define the path where all repos must be and the code to implement it

--- README.md
+++ README.md
@@ -6,10 +6,11 @@ This script requires stashy python module. You can install with pip as follows:
 ```
 pip install -r /path/to/requirements.txt --install-option="--user"
 ```
-You also need to add your stash USER, PASSWORD and STASH-URL:
+You also need to add your stash USER, PASSWORD, STASH_URL and GIT_DIR:
 ```python
     USER = '<USER>'
     PASSWORD = '<PASSWORD>'
-    STASH-URL = '<STASH-URL>'
+    STASH_URL = '<STASH_URL>'
+    GIT_DIR = '<GIT_DIR>'
 ```
 And that's all..
--- clone-stash-projects.py
+++ clone-stash-projects.py
@@ -2,9 +2,8 @@
 # -*- coding: utf_8 -*-
 
 """Module name
-Script to clone all the repos under a Stash project in local
+Script to clone all the repos under a Stash/Bitbucket project in local
 """
-
 __author__  = 'jotaele'
 __contact__ = 'antonjuanluis@gmail.com'
 __version__ = '0.1'
@@ -20,11 +19,12 @@ if __name__ == '__main__':
 
     USER = '<USER>'
     PASSWORD = '<PASSWORD>'
-    STASH-URL = '<STASH-URL>'
+    STASH_URL = '<STASH_URL>'
+    GIT_DIR = '<GIT_DIR>'
 
     l = []
 
-    stash = stashy.connect(STASH-URL, USER, PASSWORD)
+    stash = stashy.connect(STASH_URL, USER, PASSWORD)
     for p in stash.projects.list():
         d = { k: p[k] for k in ['name', 'key'] }
         l.append(d)
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     while True:
         for a, b in enumerate([e['name'] for e in l], 1):
             print('{} - {}'.format(a, b))
-        
+
         answer = input('Please choose one Project: ')
         try:
             i = int(answer)
@@ -42,7 +42,7 @@ if __name__ == '__main__':
                 continue
             break
         except ValueError:
-            print("That's not an a valid number!")     
+            print("That's not an a valid number!")
 
     proj = l[i - 1]
 
@@ -52,20 +52,24 @@ if __name__ == '__main__':
     if answer2 == "n":
         sys.exit(0)
 
-    if not os.path.isdir(proj['name']):
-        os.makedirs(proj['name'])
 
-    os.chdir(proj['name'])
+    try:
+        os.chdir(os.path.expanduser(GIT_DIR))
+        if not os.path.isdir(proj['name']):
+            os.makedirs(proj['name'])
+            print("Creating: ", proj['name'])
+        os.chdir(proj['name'])
+    except Exception as exc:
+        print(exc)
+        sys.exit(1)
 
     for repo in stash.projects[proj['key']].repos.list():
         for url in repo["links"]["clone"]:
-            if (url["name"] == "ssh"):
+            if url["name"] == "ssh":
                 if not os.path.isdir(repo['slug']):
-                    print("Cloning %s" % repo['slug'])
                     os.system("git clone %s" % url['href'])
                 else:
                     print("Pulling %s" % repo['slug'])
                     os.system("git -C %s pull" % repo['slug'])
-                break
-
+                print("---")
     sys.exit(0)
